dfb5670e73c983f2b24f78f8b8ae84469e6cabee
HyunSeongLee <devhyun05@gmail.com>
2023-09-16 22:39:40 -0400

    Projects page layout updated

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,26 +3,7 @@ const Main = () => {
     return (
 
         <>  
-            <video src={"/video/space.mp4"} autoPlay loop muted
-             style={{position: 'absolute', 
-                     objectFit: 'cover',
-                     width: '100vw',
-                     height: '100vh',
-                     zIndex: '-1'}}>
 
-            </video>
-            <header>
-
-                    <nav style={{display: 'flex', flexDirection: 'row'}}>
-                        <h2 style={{marginLeft: '2%', color:'#F0F0F0'}}>Codniverse</h2>
-                        <ul style={{display: 'flex', flexDirection: 'row',width: '100%', justifyContent: 'center', alignItems: 'center', gap: '200px', fontSize: '20px', listStyle: 'none'}}>
-                            <li><a href="/about">About</a></li>
-                            <li><a href="/resume">Resume</a></li>
-                            <li><a href="/projects">Projects</a></li>
-                            <li><a href="/contact">Contact</a></li>
-                        </ul>
-                    </nav>
-            </header>
         </>
 
     )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,17 @@
 const Projects = () => {
     return (
         <>  
-            <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '40px'}}>
-                <a href="https://weatherclothesrecommendation-67b3a3b0816e.herokuapp.com/">
-                    <img src="/img/weatherapp-preview.png" style={{borderRadius: '5px'}} alt="weatherapp preview" width="200px" height="200px"/> 
-                </a>
-                <a href="https://playpal.netlify.app/">
-                    <img src="/img/playpal.png" style={{borderRadius: '5px'}} alt="weatherapp preview" width="200px" height="200px"/> 
-                </a>
+            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                <h1 style={{color: '#F0F0F0'}}>Web Projects</h1>
+                <div style={{display: 'flex', flexDirection:'row', gap: '30px'}}>
+                    <a href="https://weatherclothesrecommendation-67b3a3b0816e.herokuapp.com/">
+                        <img src="/img/weatherapp-preview.png" style={{borderRadius: '5px'}} alt="weatherapp preview" width="300px" height="200px"/> 
+                    </a>
+                    <a href="https://playpal.netlify.app/">
+                        <img src="/img/playpal.png" style={{borderRadius: '5px'}} alt="weatherapp preview" width="300px" height="200px"/> 
+                    </a>
+                </div>
+                <h1 style={{color: '#F0F0F0'}}>Mobile Projects</h1>
             </div>
         </>
     )
