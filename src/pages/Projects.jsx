2667b3c7733bf76972b43a61a03d6a87698f8178
HyunSeongLee <devhyun05@gmail.com>
2023-09-16 23:43:22 -0400

    project lists position centered

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,8 +2,8 @@ const Projects = () => {
     return (
         <>  
             <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-                <h1 style={{color: '#F0F0F0'}}>Web Projects</h1>
-                <div style={{display: 'flex', flexDirection:'row', gap: '30px'}}>
+                <h1 style={{color: '#F0F0F0', textAlign:'center'}}>Web Projects</h1>
+                <div style={{display: 'flex', flexDirection:'row', justifyContent:'center', gap: '30px'}}>
                     <a href="https://weatherclothesrecommendation-67b3a3b0816e.herokuapp.com/">
                         <img src="/img/weatherapp-preview.png" style={{borderRadius: '5px'}} alt="weatherapp preview" width="300px" height="200px"/> 
                     </a>
@@ -11,7 +11,10 @@ const Projects = () => {
                         <img src="/img/playpal.png" style={{borderRadius: '5px'}} alt="weatherapp preview" width="300px" height="200px"/> 
                     </a>
                 </div>
-                <h1 style={{color: '#F0F0F0'}}>Mobile Projects</h1>
+                <div>
+                <h1 style={{color: '#F0F0F0', textAlign: 'center'}}>Mobile Projects</h1>
+                </div>
+
             </div>
         </>
     )
