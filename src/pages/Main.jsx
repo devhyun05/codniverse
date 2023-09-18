5bf244a8f5a9daccd1cb0ff6c42d5d9e0a865b45
HyunSeongLee <devhyun05@gmail.com>
2023-09-18 01:04:04 -0400

    Main page personal description added

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,20 @@
 import '../App.css'; 
+
 const Main = () => {
     return (
 
         <>  
-
+            <div>
+                <div className='fade-in-text' style={{color: '#F0F0F0', margin: '18% 0 0 18%'}}>
+                    <h1>Hello, I'm Hyun Seong Lee</h1>
+                    <h2>Full Stack, backend developer</h2>
+                </div>
+                <div style={{marginLeft: '60%'}}>
+                {/* <img src="/img/sitting-person.png" style={{position: 'absolute', top: '12%', right: '14%'}}alt="sitting person"/> */}
+                {/* <img className='face-in-text' src="/img/earth.png" style={{position: 'absolute', top: '35%', zIndex: '-1'}} alt="neptune planet" width="300px" height="300px"/>  */}
+             
+                </div>
+            </div>
         </>
 
     )
