4f81973faf0941935d00a5cb557a45f4ad33007a
HyunSeongLee <devhyun05@gmail.com>
2023-09-25 17:29:43 -0400

    main page animation with image

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,38 @@
 import '../App.css'; 
+import { useState, useEffect } from 'react'; 
+
 
 const Main = () => {
-    return (
 
+    const [timerToPrintTextOrImage, setTimerToPrintTextOrImage] = useState(false); 
+
+
+    useEffect(() => {
+        setTimeout(() => {
+          setTimerToPrintTextOrImage(true);
+        }, 4000); // Set timer to 3 seconds
+      }, []);
+    return (
         <>  
-            <div>
-                <div className='fade-in-text' style={{color: '#F0F0F0', margin: '18% 0 0 18%'}}>
+            <div style={{display: 'flex', 
+                         flexDirection: 'row', 
+                         padding: '17% 0 0 20%',
+                         gap: '150px',
+                         color: '#F0F0F0'}}>
+                <div className='fade-in-text' >
                     <h1>Hello, I'm Hyun Seong Lee</h1>
                     <h2>Full Stack, backend developer</h2>
                 </div>
-                <div style={{marginLeft: '60%'}}>
-                {/* <img src="/img/sitting-person.png" style={{position: 'absolute', top: '12%', right: '14%'}}alt="sitting person"/> */}
-                {/* <img className='face-in-text' src="/img/earth.png" style={{position: 'absolute', top: '35%', zIndex: '-1'}} alt="neptune planet" width="300px" height="300px"/>  */}
-             
+                <div className="anim-typewriter" style={{color: '#5CD2E6',
+                                                         width: '50%', 
+                                                         fontSize: '50px'}}>
+                    {!timerToPrintTextOrImage ?  
+                            '<div>Earth</div>' 
+                           : 
+                            <img src="../img/earth.png" alt="Earth"/>}
+                    
+       
+                {/* <img className='face-in-text' src="/img/earth.png" style={{position: 'absolute', top: '35%', zIndex: '-1'}} alt="neptune planet" width="300px" height="300px"/>  */}       
                 </div>
             </div>
         </>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,24 +2,10 @@ const Projects = () => {
     return (
         <>  
             <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '8%'}}>
-                    <ul style={{display: 'flex', 
-                                flexDirection: 'row',
-                                width: '100%', 
-                                justifyContent: 'center', 
-                                alignItems: 'center', 
-                                gap: '100px', 
-                                color: '#F0F0F0', 
-                                fontSize: '20px', 
-                                fontWeight: 'bold',
-                                listStyle: 'none'}}>
-                        <li>ALL</li>
-                        <li>REACT-JS / NODE</li>
-                        <li>REACT-JS / JAVA-SPRING</li>
-                        <li>REACT-NATIVE / C#</li>
-                    </ul>
+                    <h1 style={{textAlign: 'center', color: '#F0F0F0'}}>Projects</h1>
 
 
-                <div style={{display: 'flex', flexDirection:'row', justifyContent:'center', gap: '100px'}}>
+                <div style={{display: 'flex', flexDirection:'row', justifyContent:'center'}}>
                     <div className='project-item'>
                         <img src="/img/weatherapp-preview.png" style={{borderRadius: '5px'}} alt="weatherapp preview" width="400px" height="250px"/> 
                         <div className="content">
