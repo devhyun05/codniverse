e78282bd2aa5c836d1b46fc8abf30956dd0fc8f5
HyunSeongLee <devhyun05@gmail.com>
2023-09-25 20:41:09 -0400

    remove about page

--- src/pages/components/NavigationBar.jsx
+++ src/pages/components/NavigationBar.jsx
@@ -18,7 +18,6 @@ const navigationBar = () => {
                 <a href="/" style={{textDecoration: 'none', color:'#F0F0F0'}}>Codniverse</a>
             </h2>
             <ul style={{display: 'flex', flexDirection: 'row',width: '100%', justifyContent: 'center', alignItems: 'center', gap: '200px', fontSize: '20px', listStyle: 'none'}}>
-                <li><a className="nav-link" href="/about">About</a></li>
                 <li><a className="nav-link" href="/resume">Blog</a></li>
                 <li><a className="nav-link" href="/projects">Projects</a></li>
                 <li><a className="nav-link" href="/contact">Contact</a></li>
